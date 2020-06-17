b7abb12e95e51eab8f5b1687360dd4347a5a490d
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-06-17 12:46:08 -0400

    part 1: set up of console complete

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -13,5 +13,5 @@ class Reading
     def self.all
         @@all 
     end 
-    
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,21 @@
 require_relative '../config/environment.rb'
 
+###B BOOKS ###
+b1 = Book.new("Harry Potter")
+b2 = Book.new("Junie B Jones")
+b3 = Book.new("Cam Janses")
+b4 = Book.new("Magic Tree House")
 
-# You may use this file to test your OO Ruby code
+### USERS ###
+u1 = User.new("Lily")
+u2 = User.new("Jose")
+u3 = User.new("Sue")
 
+### READINGS ###
+r1 = Reading.new(u1,b1)
+r2 = Reading.new(u2,b2)
+r3 = Reading.new(u3,b3)
+r4 = Reading.new(u3,b4)
 
 
 
