1e3a72a58683a1e01299908c19bf33ef3a6d69bf
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-06-17 12:40:06 -0400

    part 1: setting attributes complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,15 @@
 class Book
+
+    attr_reader :title #reader because title cannot be changed 
+
+    @@all = []
+
     def initialize(title)
-        
+        @title = title
+        @@all << self 
     end
+
+    def self.all
+        @@all 
+    end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User
+
+    attr_accessor :name #accessor so we can read the user name AND change it 
+    
+    @@all = []
+
     def initialize(name)
-        
+        @name = name 
+        @@all << self  
     end
+
+    def self.all
+        @@all 
+    end 
+
 end
